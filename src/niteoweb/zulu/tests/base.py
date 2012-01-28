46464fedf13fd6cd0a37a77240981a649db48465
Nejc Zupan <nejc.zupan@gmail.com>
2012-01-28 18:41:58 +0100

    sort imports

--- src/niteoweb/zulu/tests/base.py
+++ src/niteoweb/zulu/tests/base.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 """Base module for unittesting."""
 
-import unittest2 as unittest
-
+from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
+from plone.app.testing import login
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import TEST_USER_ID
-from plone.app.testing import login
 from plone.app.testing import setRoles
-from plone.app.testing import applyProfile
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 from plone.testing import z2
+
+import unittest2 as unittest
 
 
 class NiteowebZuluLayer(PloneSandboxLayer):
