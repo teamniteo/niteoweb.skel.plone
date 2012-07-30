98dde4399cb71b3c97b6f89314998f031ebd6bdd
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-30 14:08:48 +0200

    pep8 of setup.py

--- setup.py
+++ setup.py
@@ -13,60 +13,64 @@ def read(*rnames):
 
 version = read('src', 'niteoweb', 'zulu', 'version.txt').strip()
 
-setup(name='niteoweb.zulu',
-      version=version,
-      description="Enter description of what this project is all about.",
-      long_description=read('README.rst') +
-                       read('docs', 'HISTORY.rst') +
-                       read('docs', 'LICENSE.rst'),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-      ],
-      keywords='Plone Python',
-      author='NiteoWeb Ltd.',
-      author_email='info@niteoweb.com',
-      url='http://www.niteoweb.com',
-      license='BSD',
-      packages=find_packages('src', exclude=['ez_setup']),
-      namespace_packages=['niteoweb'],
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'five.pt',
-          'niteoweb.fabfile',
-          'niteoweb.loginas',
-          'Pillow',
-          'Plone',
-          'plone.app.caching',
-          'plone.app.theming',
-          'setuptools',
-          'z3c.jbot',
-      ],
-      extras_require={
-          'test': [
-              'mock',
-              'plone.app.testing',
-              'unittest2',
-          ],
-          'develop': [
-              'jarn.mkrelease',
-              'pep8',
-              'plone.app.debugtoolbar',
-              'plone.reload',
-              'Products.Clouseau',
-              'Products.DocFinderTab',
-              'Products.PDBDebugMode',
-              'Products.PrintingMailHost',
-              'setuptools-flakes',
-              'zest.releaser',
-              'zptlint',
-          ],
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
+long_description = \
+    read('README.rst') + \
+    read('docs', 'HISTORY.rst') + \
+    read('docs', 'LICENSE.rst')
+
+setup(
+    name='niteoweb.zulu',
+    version=version,
+    description="Enter description of what this project is all about.",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],
+    keywords='Plone Python',
+    author='NiteoWeb Ltd.',
+    author_email='info@niteoweb.com',
+    url='http://www.niteoweb.com',
+    license='BSD',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['niteoweb'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'five.pt',
+        'niteoweb.fabfile',
+        'niteoweb.loginas',
+        'Pillow',
+        'Plone',
+        'plone.app.caching',
+        'plone.app.theming',
+        'setuptools',
+        'z3c.jbot',
+    ],
+    extras_require={
+        'test': [
+            'mock',
+            'plone.app.testing',
+            'unittest2',
+        ],
+        'develop': [
+            'jarn.mkrelease',
+            'pep8',
+            'plone.app.debugtoolbar',
+            'plone.reload',
+            'Products.Clouseau',
+            'Products.DocFinderTab',
+            'Products.PDBDebugMode',
+            'Products.PrintingMailHost',
+            'setuptools-flakes',
+            'zest.releaser',
+            'zptlint',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
 )
